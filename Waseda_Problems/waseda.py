319abdbe45fbf95ba6b463bc8a664cb81b956425
yuyu5510 <yuyu5510.chara@icloud.com>
2022-03-28 23:49:18 +0900

    Add_files

---
+++ Waseda_Problems/waseda.py
@@ -0,0 +1,162 @@
+# coding: utf-8
+from nltk.corpus import wordnet as wn
+#同じ階層にあるword_use.pyをimport
+from word_use import word_use
+import numpy as np
+
+
+class Waseda:
+    def __init__(self):
+        #word_useは先頭のアルファベットによって振り分けている二重のリスト
+        #26以下の数字をランダムに選択
+        rand = np.random.randint(26)
+        rand2 = np.random.randint(len(word_use[rand]))
+        #答え
+        self.ans = word_use[rand][rand2]
+        rand3 = np.random.randint(len(word_use[rand]))
+        rand4 = np.random.randint(len(word_use[rand]))
+        #選択肢を存在する単語から2つ生成
+        self.dummy = word_use[rand][rand3]
+        self.dummy2 = word_use[rand][rand4]
+        #選択肢
+        self.choices = []
+        #例文を入れる
+        self.examples= []
+
+        synsets = wn.synsets(self.ans)
+        #選んだ単語の定義と例文を呼び出して追加
+        for i in range(len(synsets)):
+           if self.ans in str(synsets[i]):
+        #teachの例文でtaughtが入ってたりすると問題としてふさわしくないし面倒なのでそういうのは切り捨てる
+               ok = False
+               for j in range(len(synsets[i].examples())):
+                   if ok: break
+                   if self.ans in synsets[i].examples()[j]:
+                        exam = synsets[i].definition()
+                        exam += ';  '
+                        exam += synsets[i].examples()[j]
+                        self.examples.append(exam)
+                        ok = True
+
+        #例文が複数ある場合、毎回固定だとつまらないのでシャッフルしておく
+        np.random.shuffle(self.examples)
+        #例文がひとつもなかった時用に定義だけ追加　もし例文があれば次のpopでこれは弾かれる
+        for i in range(len(synsets)):
+            if self.ans in str(synsets[i]):
+                self.examples.append(synsets[i].definition())
+
+        #本家は2個なので2個以上ある場合は2個になるまで削除
+        while len(self.examples) > 2:
+            self.examples.pop()
+
+    #早稲田方式に単語を変換
+    def convert_waseda(self, s):
+        one = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+        two = ['h', 'i', 'j', 'k', 'l', 'm']
+        three = ['n', 'o', 'p','q', 'r', 's']
+        four = ['t', 'u', 'v', 'w', 'x', 'y', 'z']
+
+        conv = ""
+        conv += s[0]
+        for i in range(1,len(s)):
+            if s[i] in one:
+                conv += '1'
+            elif s[i] in two:
+                conv += '2'
+            elif s[i] in three:
+                conv += '3'
+            elif s[i] in four:
+                conv += '4'
+        
+        if not conv in self.choices:
+            self.choices.append(conv)
+        #もし選択肢に同一のがあればランダムに生成
+        else:
+            self.random_choice()
+        return conv
+    
+    def random_choice(self):
+        rand_choice = self.ans[0]
+        #長さが短すぎると嘘がわかるので最低4文字以上にする
+        rand_num1 = np.random.randint(3,8)
+        for i in range(rand_num1):
+            rand_choice += str(np.random.randint(4)+1)
+        #重複がなければ追加あればもう一回呼ぶ(確率的には何回も完全一致はしないはずなので)
+        if not rand_choice in self.choices:
+            self.choices.append(rand_choice)
+        else :
+            self.random_choice()
+    
+    def solve(self):
+        print("The correct answer is {}.".format(self.ans))
+            
+
+    def problem(self):
+        #例文の中にある正解の単語を(   )で置き換える
+        for i in range(len(self.examples)):
+            tmp = '{}. {}.'.format(i+1, self.examples[i].replace(self.ans, '(     )'))
+            print(tmp)
+        #答えが早稲田方式に変換されたのを持っておく
+        ans_converted = self.convert_waseda(self.ans)
+        self.convert_waseda(self.dummy)
+        self.convert_waseda(self.dummy2)
+        self.random_choice()
+        np.random.shuffle(self.choices)
+
+        #print()で改行する蛮行をする
+        print()
+        print()
+
+
+        for i in range(len(self.choices)):
+            print('{}. {}'.format(i+1, self.choices[i]))
+
+        ok = True
+        
+        while ok:
+            print('\nChoose answer: ')
+            #例外処理
+            try:
+                kaitou = int(input())
+            except KeyboardInterrupt:
+                return
+            except:
+                print('Invalid input. Try again.')
+                continue
+
+            #1,2,3,4以外の数字が選ばれた際もう一回入力させる
+            if kaitou <= 0 or kaitou > 4:
+                print('Invalid input. Try again.')
+                continue
+
+            kaitou -= 1
+            if ans_converted == self.choices[kaitou]:
+                print('Your answer is collect!!')
+                self.solve()
+                ok = False
+            else:
+                print('Your answer is wrong')
+                print('Do you continue ? please answer yes or no.')
+                cont = str(input())
+                if cont == "YES" or cont == "Yes" or cont == "yes" or cont == "y" or cont == "Y":
+                    continue
+                #yes以外では終了
+                else:
+                    ok = False
+                #答えを見せる
+                self.solve()
+
+a = Waseda()
+a.problem()
+
+        
+
+
+        
+
+            
+        
+        
+
+
+        
---
+++ Waseda_Problems/word_use.py
@@ -0,0 +1,77 @@
+word_use = [['abandon', 'ability', 'able', 'abnormal', 'aboard', 'abolish', 'abolition', 'abortion', 'abound', 'abruptly', 
+'absolute', 'absolutely', 'absorb', 'absorption', 'abstain', 'abstract', 'absurd', 'abundance', 'abundant', 'abuse', 'academic',
+'accelerate', 'accent', 'accept', 'acceptance', 'access', 'accessible', 'accident', 'accompany', 'accomplish', 'accord', 'adapt',
+'add', 'addict', 'addictive', 'addition', 'adjust', 'admit', 'advent', 'adverse', 'advertise', 'affect', 'agenda', 'agent', 'akin', 
+'alcohol', 'alien', 'allot', 'analogy', 'ancient', 'ankle', 'annoy', 'annual', 'apart', 'ape', 'approach', 'approval', 'armed', 'article', 
+'ascent', 'aside', 'aspect', 'asset', 'assign', 'athlete', 'atom', 'author', 'award', 'awful'],
+
+['ban', 'barrier', 'basis', 'bear', 'beast', 'beat', 'behave', 'bend', 'beneath', 'besides', 'bestow', 'betray',
+ 'beverage', 'bias', 'biology', 'bitter', 'blame', 'bleed', 'bless', 'blink', 'bloom', 'blur', 'board', 'boast',
+'bold', 'bomb', 'boost', 'bore', 'boring', 'boss', 'bother'],
+
+['cafeteria', 'cage', 'career', 'case', 'cell', 'check', 'cause', 'change', 'charm', 'civil', 'clerk', 'collapse', 'collect', 'comment',
+'communicate', 'communism', 'company', 'complex', 'comprise', 'confident', 'confuse', 'connect', 'consent', 'constant', 'content', 'control', 
+'convert', 'corrupt', 'cover', 'criminal', 'crisis', 'crude', 'custom'],
+
+['debt', 'decide', 'defeat', 'defend', 'define', 'demand', 'deny', 'descend', 'describe', 'desert', 'despair', 'desperate', 
+'destiny', 'determine', 'devote', 'diet', 'differ', 'difference', 'dignity', 'diplomat', 'discourse', 'discuss', 
+'disease', 'disorder', 'distinct', 'diverse', 'divide', 'divine', 'do', 'donate', 'due', 'duty'],
+
+['eager', 'economy', 'edit', 'effect', 'elect', 'element', 'elite', 'emerge', 'emit', 'enable', 'endow', 'endure', 'enforce',
+ 'enhance', 'entire', 'envy', 'era', 'erection', 'erupt', 'ethics', 'evade', 'event', 'excel', 'excuse', 'exert', 'exist', 
+ 'expand', 'expire', 'extra'],
+
+['face', 'fade', 'fail', 'faint', 'false', 'fame', 'favor', 'feat', 'feed', 'female', 'fever', 'figure', 
+'fill', 'fine', 'fit', 'flesh', 'fold', 'force', 'forgive', 'free', 'frequent', 'fuel', 'fulfill', 'fully', 'furious', 'fuss'],
+
+['gain', 'gene', 'genetic', 'geology', 'gift', 'given', 'glow', 'govern', 'grain', 'grasp', 'grave', 'greet'],
+
+['halt', 'harm', 'hazard', 'heap', 'heretic', 'hire', 'holy', 'hook', 'horrible', 'huge', 'hydrogen'],
+
+['ideal', 'idle', 'ignore', 'illegal', 'imitate', 'immune', 'impact', 'imply', 'incident', 'include',
+'increase', 'index', 'induce', 'infant', 'infer', 'infinite', 'inhabit', 'inherit', 'innate', 'insect',
+'insert', 'insight', 'insist', 'instruct', 'inspire', 'introduce', 'involve', 'isolate', 'item'],
+
+['jealous', 'judge', 'justice', 'justify', 'juvenile'],
+
+['keen', 'knee', 'know', 'knowledge'],
+
+['label', 'labor', 'lack', 'lately', 'lay', 'lead', 'leap', 'legal', 'leisure', 'letter', 'liable', 
+ 'lie', 'limb', 'literate', 'local', 'logic', 'loose', 'lump', 'luxury'],
+
+['maintain', 'make', 'male', 'manifest', 'mark', 'match', 'mean', 'means', 'mental', 'mention',
+ 'method', 'mine', 'minor', 'minute', 'mode', 'modern', 'mood', 'motivation', 'murder', 'myth'],
+
+['naïve', 'naked', 'namely', 'native', 'natural', 'negative', 'nerve', 'nod', 'norm', 'novel', 'novice', 'nutrient', 'nutrition'],
+
+['obese', 'obey', 'object', 'obscure', 'observe', 'obsolete', 'obstacle', 'obtain', 'occupy', 'occur', 'odd', 'offend', 
+'official', 'operate', 'oppose', 'option', 'oral', 'order', 'origin', 'oxygen', 'overcome'],
+
+['palm', 'paradox', 'parallel', 'partly', 'party', 'passion', 'patient', 'pave', 'peer', 'period', 'persist',
+ 'personal', 'persuade', 'phase', 'physics', 'pick', 'pity', 'point', 'polar', 'pole', 'poll', 'pollute', 
+ 'positive', 'pour', 'precise', 'prefer', 'pregnant', 'prevail', 'profit', 'project', 'provide', 'pursue'],
+
+['qualification', 'qualify', 'quality', 'quantity', 'quarrel', 'quarter', 'queue', 'quote'],
+
+['race', 'racial', 'raise', 'range', 'rank', 'rare', 'rate', 'raw', 'react', 'real', 'rebel', 'recipe', 'recite', 
+'recover', 'reduce', 'refine', 'reform', 'relation', 'relative', 'relevant', 'relief', 'rely',
+'remove', 'resent', 'resolve', 'rest', 'restrict', 'retail', 'retreat', 'revenue', 'rob', 'rot', 'rule'],
+
+['sacred', 'safety', 'saint', 'save', 'say', 'scratch', 'secure', 'self', 'shallow', 'shame', 'sigh',
+'sinful', 'slap', 'smart', 'snore', 'soak', 'sober', 'social', 'sole', 'solid', 'sort', 'spell', 'spot', 
+'stare', 'statue', 'status', 'stiff', 'stir', 'stock', 'story', 'stir', 'static', 'strain', 
+'stingy', 'story', 'strict', 'submit', 'subtle', 'superb', 'survey', 'sword'],
+
+['tackle', 'task', 'tame', 'tax', 'tactics', 'tear', 'tend', 'term', 'test', 'theory', 
+'thus', 'tiny', 'trend', 'trade', 'total', 'toke', 'transform', 'transit', 'trash',
+'trend', 'treaty', 'trust', 'tutor', 'turn', 'tutor', 'twist', 'tyrant'],
+
+['underlie', 'unfold', 'union', 'unite', 'universe', 'up', 'urban', 'urge', 'utmost', 'utterly'],
+
+['valid', 'vanish', 'vary', 'verb', 'view', 'vigor', 'virtue', 'vision', 'violent', 'vivid', 'volume', 'vote', 'voyage'],
+
+['wage', 'wait', 'wander', 'waste', 'weapon', 'wear', 'well', 'wheat', 'wheel', 'will', 'wit', 'witch', 'withdraw', 'wither', 'witness', 'witty', 'wrist'],
+
+['yawn', 'year', 'yell', 'yield'],
+
+['zeal', 'zealous', 'zero', 'zip']]
